6a65cbe1930acecc37f64b6bb394bdbdd12a0a44
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-11 23:42:52 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,95 @@
+print("Hello world")
+
+## variables
+
+#strings
+first_name="bro"
+print(f"hello {first_name}")
+
+#integers
+age=26
+print(f"your age id: {age}")
+
+#float
+price=23.34
+print(f"price is {price}")
+
+#boolean
+is_student=True #T or F should be capital
+print(f"are you a student?: {is_student}")
+
+for_sale=True
+if for_sale:
+    print(f"that item is for sale")
+else:
+    print(f"not for sale")
+
+
+##Typecasting
+name="bro code"
+print(type(name))
+gpa=6.3
+gpa=int(gpa)
+print(type(gpa))
+age=str(age)
+print(type(age))
+
+##input()
+
+# name=input("whts your name:?")
+# print(f"my name is: {name}")
+# age=int(input("your aage:? "))
+# age+=1
+# print(f"your age is: {age}")
+
+##Exercise
+# item=input("what do u want to buy:? ")
+# price=float(input("what is the price:? "))
+# quantity=int(input("what the quantity:? "))
+# total=price*quantity
+
+
+# print(f"you have bought: {quantity}x{item}/s")
+# print(f"your total is: ${total}")
+
+##some basics
+# import math
+
+# print(math.pi)
+# print(math.e)
+# x=9
+# result=math.sqrt(x)
+# print(f"{result}")
+
+##exercises
+# import math
+
+# radius=float(input("enter the radius: "))
+# circumference=2*math.pi*radius
+# print(f"circumference of the circle is: {round(circumference,2)}cm")
+
+
+
+# import math
+# a=float(input("enter the first: "))
+# b=float(input("enter the second: "))
+
+# c=math.sqrt(pow(a,2)+pow(b,2))
+
+# print(f"side c is: {round(c,2)}")
+
+
+##ifelse
+
+
+
+
+
+
+
+
+
+
+
+
+
