8fc4a3014dc5d6fe325c33185bc2ebad87061578
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-14 23:41:01 +0530

    added

--- basics.py
+++ basics.py
@@ -470,6 +470,30 @@ print(type(age))
 # print(positives_num)
 
 
+##SWITCHCASE##
+
+# def day_of_week(day):
+#     match day:
+#         case 1:
+#             return "It is sunday"
+#         case 2:
+#             return "It is moonday"
+#         case 3:
+#             return "It is Tuesday"
+#         case 4:
+#             return "It is wednesday"
+#         case 5:
+#             return "It is Thursday"
+#         case 6:
+#             return "It is Friday"
+#         case 7:
+#             return "It is Saturday"
+#         case _:
+#             return "Not a valid day"
+        
+
+
+# print(day_of_week(1))        
 
 
 
