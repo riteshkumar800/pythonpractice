1f6c79b50d46d5f3a6339fc52413cdfab511eb31
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-17 23:43:47 +0530

    added

--- pythonobjectorientedprogramming.py
+++ pythonobjectorientedprogramming.py
@@ -5,3 +5,11 @@ class Car:
         self.color=color
         self.for_sale=for_sale
 car1=Car("Mustang",2024,"red",False)
+car2=Car("corvette",2025,"blue",True)
+car3=Car("Charger",2026,"yellow",True)
+
+print(car3.model)
+print(car3.color)
+print(car3.year)
+print(car3.for_sale)
+print(car2.model)
