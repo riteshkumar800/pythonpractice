3837bbc7c7960b7cf840fd1105446c4e86dd1e58
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-17 23:48:48 +0530

    new

--- pythonobjectorientedprogramming.py
+++ pythonobjectorientedprogramming.py
@@ -1,9 +1,5 @@
-class Car:
-    def __init__(self,model,year,color,for_sale):
-        self.model=model
-        self.year=year
-        self.color=color
-        self.for_sale=for_sale
+from car import Car
+
 car1=Car("Mustang",2024,"red",False)
 car2=Car("corvette",2025,"blue",True)
 car3=Car("Charger",2026,"yellow",True)
