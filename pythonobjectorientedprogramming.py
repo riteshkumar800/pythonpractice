a2bbdbe44a24ad313b25f2e03453c7c9074dc81b
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-17 23:45:18 +0530

    added

--- pythonobjectorientedprogramming.py
+++ pythonobjectorientedprogramming.py
@@ -1,5 +1,5 @@
 class Car:
-    def __int__(self,model,year,color,for_sale):
+    def __init__(self,model,year,color,for_sale):
         self.model=model
         self.year=year
         self.color=color
