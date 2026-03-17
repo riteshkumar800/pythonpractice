863db34a080d4f1493c5bccb31fdb6e39ddc4aa7
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-17 23:39:14 +0530

    added

---
+++ pythonobjectorientedprogramming.py
@@ -0,0 +1,7 @@
+class Car:
+    def __int__(self,model,year,color,for_sale):
+        self.model=model
+        self.year=year
+        self.color=color
+        self.for_sale=for_sale
+car1=Car("Mustang",2024,"red",False)
