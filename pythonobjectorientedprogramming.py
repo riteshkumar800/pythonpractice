9ac4169d3b6209585bd7f1e815677fca870df46a
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-03-22 01:01:28 +0530

    new added code

--- pythonobjectorientedprogramming.py
+++ pythonobjectorientedprogramming.py
@@ -81,17 +81,101 @@
 
 #############################################################################################################################
 #super()
-class Shape:
-    def __init__(self,color,is_filled):
-        self.color=color
-        self.is_filled=is_filled
+# class Shape:
+#     def __init__(self,color,is_filled):
+#         self.color=color
+#         self.is_filled=is_filled
+
+#     def describe(self):
+#         print(f"it is {self.color} and {'filled' if self.is_filled else 'not filled'}")
 
 
-class Circle(Shape):
-    def __init__(self,color,is_filled,radius):
-        super().__init__(color,is_filled)
-        self.radius=radius
+
+
+# class Circle(Shape):
+#     def __init__(self,color,is_filled,radius):
+#         super().__init__(color,is_filled)
+#         self.radius=radius
+
+#     def describe(self):
+#         print(f"it is a ircle with an area of {3.14*self.radius*self.radius}m^2")
+#         super().describe()
+
         
+# class Square(Shape):
+#     def __init__(self,color,is_filled,width):
+#         super().__init__(color,is_filled)
+#         self.width=width
+# class Triangle(Shape):
+#     def __init__(self,color,is_filled,width,height):
+#         super().__init__(color,is_filled)
+#         self.width=width
+#         self.height=height
+
+
+# circle=Circle(color="red",is_filled=True,radius=2)
+# square=Square("blue",False,6)
+
+# print(square.width)
+# # square.describe()
+# circle.describe()
+
+#############################################################################################################################
+##POLYMORPHISM
+# from abc import ABC, abstractmethod
+
+# class Shape:
+#     @abstractmethod
+#     def area(self):
+#         pass
+
+# class Circle(Shape):
+#     def __init__(self,radius):
+#         self.radius=radius
+
+#     def area(self):
+#         return 3.14*self.radius**2
+    
+
+
+# class Square(Shape):
+#     def __init__(self,side):
+#         self.side=side
+
+#     def area(self):
+#         return self.side**2
+    
+
+
+# class Triangle(Shape):
+#     def __init__(self,base,height):
+#         self.base=base
+#         self.height=height
+
+#     def area(self):
+#         return self.base*self.height*0.5
+    
+
+# class Pizza(Circle):
+#     def __init__(self,topping,radius):
+#         self.topping=topping
+#         self.radius=radius
+
+
+
+# shapes=[Circle(4),Square(5),Triangle(10,8),Pizza("perponi",15)]
+
+# for shape in shapes:
+#     print(f"{shape.area()}cm^2")
+
+#############################################################################################################################
+
+    
+
+
+
+
+
 
 
 
